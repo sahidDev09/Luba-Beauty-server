5b5ad65828b70a63739d6ce75a2e57e0b7eaf39a
sahidDev09 <iamsahidofficial99@gmail.com>
2024-08-16 18:14:37 +0600

    deploy

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     console.log("Connected to MongoDB successfully!");
 
     // All-collection
